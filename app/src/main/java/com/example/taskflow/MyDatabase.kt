06d9ec8ed50b32ffa4e7d383d40ab29339441862
Mhd9771 <IT22609212@my.sliit.lk>
2024-05-10 17:07:04 +0530

    logo UI

--- app/src/main/java/com/example/taskflow/MyDatabase.kt
+++ app/src/main/java/com/example/taskflow/MyDatabase.kt
@@ -1,10 +1,9 @@
 package com.example.taskflow
-
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
-class MyDatabase(context: Context) : SQLiteOpenHelper(context, "DATABASE_NAME", null, DATABASE_VERSION) {
+class MyDatabase(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
     companion object {
         private const val DATABASE_NAME = "TaskFlow.db"
         private const val DATABASE_VERSION = 1
@@ -17,16 +16,20 @@ class MyDatabase(context: Context) : SQLiteOpenHelper(context, "DATABASE_NAME", 
     }
 
     override fun onCreate(db: SQLiteDatabase?) {
-        val query = "CREATE TABLE $TABLE_NAME " +
-                "($COLUMN_ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                "$COLUMN_TITLE TEXT, " +
-                "$COLUMN_AUTHOR TEXT, " +
-                "$COLUMN_PAGES INTEGER)"
-        db?.execSQL(query)
+        db?.let {
+            val query = "CREATE TABLE $TABLE_NAME " +
+                    "($COLUMN_ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    "$COLUMN_TITLE TEXT, " +
+                    "$COLUMN_AUTHOR TEXT, " +
+                    "$COLUMN_PAGES INTEGER)"
+            it.execSQL(query)
+        }
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
-        db?.execSQL("DROP TABLE IF EXISTS $TABLE_NAME")
-        onCreate(db)
+        db?.let {
+            it.execSQL("DROP TABLE IF EXISTS $TABLE_NAME")
+            onCreate(it)
+        }
     }
 }
--- app/src/main/java/com/example/taskflow/Starting.kt
+++ app/src/main/java/com/example/taskflow/Starting.kt
@@ -1,28 +1,28 @@
 package com.example.taskflow
+
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class MainActivity : AppCompatActivity() {
+class Starting : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_starting)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val floatingActionButton = findViewById<FloatingActionButton>(R.id.floatingActionButton)
-        floatingActionButton.setOnClickListener{
-            val intent = Intent(this@MainActivity, Add::class.java)
+        val logo = findViewById<ImageButton>(R.id.logo)
+        logo.setOnClickListener {
+            val intent = Intent(this@Starting,MainActivity::class.java)
             startActivity(intent)
         }
-
     }
 }
